ab6ba97a5f279387ba5c727e199d35c17aeec1eb
Damian Krychowski <damian.krychowski@gmail.com>
2016-03-06 20:17:17 +0100

    build.ps1 correction

--- Cake/build.ps1
+++ Cake/build.ps1
@@ -29,7 +29,7 @@ http://cakebuild.net
 #>
 
 Param(
-    [string]$Script = "build.cake",
+    [string]$Script = "./build.cake",
     [string]$Target = "Default",
     [string]$Configuration = "Release",
     [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
